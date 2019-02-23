b51e3c76acc14e1352c233e75cbae238bf6c3f55
Santiago <santiagomoreno60@gmail.com>
2019-02-23 17:02:28 -0500

    Finished adding all user and goal related APIs

--- app/src/main/java/com/example/budgetmate/Network/NetworkAdapters.java
+++ app/src/main/java/com/example/budgetmate/Network/NetworkAdapters.java
@@ -1,17 +1,126 @@
 package com.example.budgetmate.Network;
 
 class postRespObj {
-    public String successful;
+    public Boolean successful;
     public String error;
     public String result;
 }
 
 class RetrieveUserObj {
-    public String token;
+    private String token;
     public String action = "retrieveUser";
-
     public RetrieveUserObj(String token) {
         this.token = token;
+    }
+}
+
+class RegisterUserObj {
+    private String token;
+    private String firstName;
+    private String lastName;
+    public String action = "registerUser";
+    public RegisterUserObj(String token, String firstName, String lastName) {
+        this.token = token;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
+
+class RetrieveGoalsObj {
+    private String token;
+    public String action = "retrieveGoals";
+    public RetrieveGoalsObj(String token) {
+        this.token = token;
+    }
+}
+
+class CreateGoalObj {
+    private String token;
+    public String action = "createNewGoal";
+    private String goalName;
+    private Double goalAmount;
+    private Double goalAmountSaved;
+    public CreateGoalObj(String token, String goalName, Double goalAmount, Double goalAmountSaved) {
+        this.token = token;
+        this.goalName = goalName;
+        this.goalAmount = goalAmount;
+        this.goalAmountSaved = goalAmountSaved;
+    }
+}
+
+class DeleteGoalObj {
+    private String token;
+    public String action = "deleteGoal";
+    private String goalID;
+    public DeleteGoalObj(String token, String goalID) {
+        this.token = token;
+        this.goalID = goalID;
+    }
+}
+
+class UpdateGoalNameObj {
+    private String token;
+    public String action = "updateGoalName";
+    private String goalID;
+    private String goalName;
+    public UpdateGoalNameObj(String token, String goalID, String goalName) {
+        this.token = token;
+        this.goalID = goalID;
+        this.goalName = goalName;
+    }
+}
+
+class UpdateGoalAmountObj {
+    private String token;
+    public String action = "updateGoalAmount";
+    private String goalID;
+    private Double goalAmount;
+    public UpdateGoalAmountObj(String token, String goalID, Double goalAmount) {
+        this.token = token;
+        this.goalID = goalID;
+        this.goalAmount = goalAmount;
+    }
+}
+
+class UpdateGoalAmountSavedObj {
+    private String token;
+    public String action = "updateGoalAmountSaved";
+    private String goalID;
+    private Double goalAmountSaved;
+    public UpdateGoalAmountSavedObj(String token, String goalID, Double goalAmountSaved) {
+        this.token = token;
+        this.goalID = goalID;
+        this.goalAmountSaved = goalAmountSaved;
+    }
+}
+
+class UpdateGoalFinishedObj {
+    private String token;
+    public String action = "updateGoalFinished";
+    private String goalID;
+    public UpdateGoalFinishedObj(String token, String goalID) {
+        this.token = token;
+        this.goalID = goalID;
+    }
+}
+
+class ActivateGoalObj {
+    private String token;
+    public String action = "activateGoal";
+    private String goalID;
+    public ActivateGoalObj(String token, String goalID) {
+        this.token = token;
+        this.goalID = goalID;
+    }
+}
+
+class DisableGoalObj {
+    private String token;
+    public String action = "disableGoal";
+    private String goalID;
+    public DisableGoalObj(String token, String goalID) {
+        this.token = token;
+        this.goalID = goalID;
     }
 }
 
--- app/src/main/java/com/example/budgetmate/Network/NetworkManager.java
+++ app/src/main/java/com/example/budgetmate/Network/NetworkManager.java
@@ -1,6 +1,6 @@
 package com.example.budgetmate.Network;
 
-import com.example.budgetmate.Models.User;
+import com.example.budgetmate.Models.*;
 import com.google.gson.Gson;
 
 import java.io.IOException;
@@ -29,6 +29,10 @@ public class NetworkManager {
         }
     }
 
+    /*
+        Users
+     */
+
     public static User retrieveUser(String token) throws IOException {
         Gson gson = new Gson();
         RetrieveUserObj retrieveUserObj = new RetrieveUserObj(token);
@@ -37,5 +41,102 @@ public class NetworkManager {
         postRespObj respObj = gson.fromJson(response, postRespObj.class);
         String user = respObj.result;
         return gson.fromJson(user, User.class);
+    }
+
+    public static User registerUser(String token, String firstName, String lastName) throws IOException {
+        Gson gson = new Gson();
+        RegisterUserObj registerUserObj = new RegisterUserObj(token, firstName, lastName);
+        String registerUserJSON = gson.toJson(registerUserObj);
+        String response = makePostRequest(registerUserJSON);
+        postRespObj respObj = gson.fromJson(response, postRespObj.class);
+        String user = respObj.result;
+        return gson.fromJson(user, User.class);
+    }
+
+    /*
+        Goals
+     */
+
+    public static UserGoals retrieveUserGoals(String token) throws IOException {
+        Gson gson = new Gson();
+        RetrieveGoalsObj retrieveGoalsObj = new RetrieveGoalsObj(token);
+        String retrieveGoalsJSON = gson.toJson(retrieveGoalsObj);
+        String response = makePostRequest(retrieveGoalsJSON);
+        postRespObj respObj = gson.fromJson(response, postRespObj.class);
+        String goals = respObj.result;
+        return gson.fromJson(goals, UserGoals.class);
+    }
+
+    public static Goal createNewGoal(String token, String goalName, Double goalAmount, Double amountSaved) throws IOException {
+        Gson gson = new Gson();
+        CreateGoalObj createGoalObj = new CreateGoalObj(token, goalName, goalAmount, amountSaved);
+        String createGoalJSON = gson.toJson(createGoalObj);
+        String response = makePostRequest(createGoalJSON);
+        postRespObj respObj = gson.fromJson(response, postRespObj.class);
+        String user = respObj.result;
+        return gson.fromJson(user, Goal.class);
+    }
+
+    public static boolean deleteGoal(String token, String goalID) throws IOException {
+        Gson gson = new Gson();
+        DeleteGoalObj deleteGoalObj = new DeleteGoalObj(token, goalID);
+        String deleteGoalJSON = gson.toJson(deleteGoalObj);
+        String response = makePostRequest(deleteGoalJSON);
+        postRespObj respObj = gson.fromJson(response, postRespObj.class);
+        return respObj.successful;
+    }
+
+    public static boolean updateGoalName(String token, String goalID, String goalName) throws IOException {
+        Gson gson = new Gson();
+        UpdateGoalNameObj updateGoalNameObj = new UpdateGoalNameObj(token, goalID, goalName);
+        String updateGoalNameJSON = gson.toJson(updateGoalNameObj);
+        String response = makePostRequest(updateGoalNameJSON);
+        postRespObj respObj = gson.fromJson(response, postRespObj.class);
+        return respObj.successful;
+    }
+
+    public static boolean updateGoalAmount(String token, String goalID, Double goalAmount) throws IOException {
+        Gson gson = new Gson();
+        UpdateGoalAmountObj updateGoalAmountObj = new UpdateGoalAmountObj(token, goalID, goalAmount);
+        String updateGoalAmountJSON = gson.toJson(updateGoalAmountObj);
+        String response = makePostRequest(updateGoalAmountJSON);
+        postRespObj respObj = gson.fromJson(response, postRespObj.class);
+        return respObj.successful;
+    }
+
+    public static boolean updateGoalAmountSaved(String token, String goalID, Double goalAmountSaved) throws IOException {
+        Gson gson = new Gson();
+        UpdateGoalAmountObj updateGoalAmountSavedObj = new UpdateGoalAmountObj(token, goalID, goalAmountSaved);
+        String updateGoalAmountSavedJSON = gson.toJson(updateGoalAmountSavedObj);
+        String response = makePostRequest(updateGoalAmountSavedJSON);
+        postRespObj respObj = gson.fromJson(response, postRespObj.class);
+        return respObj.successful;
+    }
+
+    public static boolean updateGoalFinished(String token, String goalID) throws IOException {
+        Gson gson = new Gson();
+        UpdateGoalFinishedObj updateGoalFinishedObj = new UpdateGoalFinishedObj(token, goalID);
+        String updateGoalFinishedJSON = gson.toJson(updateGoalFinishedObj);
+        String response = makePostRequest(updateGoalFinishedJSON);
+        postRespObj respObj = gson.fromJson(response, postRespObj.class);
+        return respObj.successful;
+    }
+
+    public static boolean updateGoalActivate(String token, String goalID) throws IOException {
+        Gson gson = new Gson();
+        ActivateGoalObj activateGoalObj = new ActivateGoalObj(token, goalID);
+        String activateGoalJSON = gson.toJson(activateGoalObj);
+        String response = makePostRequest(activateGoalJSON);
+        postRespObj respObj = gson.fromJson(response, postRespObj.class);
+        return respObj.successful;
+    }
+
+    public static boolean updateGoalDisable(String token, String goalID) throws IOException {
+        Gson gson = new Gson();
+        DisableGoalObj disableGoalObj = new DisableGoalObj(token, goalID);
+        String disableGoalJSON = gson.toJson(disableGoalObj);
+        String response = makePostRequest(disableGoalJSON);
+        postRespObj respObj = gson.fromJson(response, postRespObj.class);
+        return respObj.successful;
     }
 }
